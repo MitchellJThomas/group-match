4f2b3744f44a796ceb3c6de5edffaab80d384105
Mitchell Thomas <mitch.thomas@gmail.com>
2013-02-27 21:52:38 -0800

    Playing around with matche

--- src/group_match/mitch/logic.clj
+++ src/group_match/mitch/logic.clj
@@ -55,8 +55,6 @@
 ;; where unique is defined as
 ;;  for group least one member of the group is different
 
-(def students [:Finley :Lark :Julie :Mitch :Gary :Jim :Yvonne :Elaine :A :B :C :D :E :F :G :H :I :J :K :L])
-
 ;; Successfull for any even divisions of ct
 (defne even-splito [ct gt out]
   ([]
@@ -71,13 +69,66 @@
      (subseto out l)
      (matche [out] ([[_ _ _ _]] succeed))))
 
+(def students [:Finley :Lark :Julie :Mitch :Gary
+               :Jim :Yvonne :Elaine :Coby :Klarisa
+               :Emily :Cora :Kevin :Heather :Rick
+               :Hazel :Beau :Duke :Scout :Happy])
 (comment
-  (run 40 [out] (groupo students out))
+  ;; Taking inspiration from http://worrydream.com/#!/LearnableProgramming, trying to "break it down" (and get funky!)
+  ;; For each session, separate the class of N students into groups of X students
+  ;; For each session, each group of students should be unique from group in the previous session
+  ;; "Unify" a class of stundents to class
+  ;; (== class [1 2 3 4 5 6 7 8 9 10 11 12 13 141 5 16 17 18 19 20])
+  ;; Break down the class into named groups
+  ;; session => (== class [( == g1 [_ _ _ _ _]) (== g2 [_ _ _ _ _]) (== g3 [_ _ _ _ _]) (== g4 [_ _ _ _ _])])
+  ;; For each named group, this group should be uniqe from the previous group
+  ;; session => (unique g1 prev-g1) (unique g2 prev-g2) (unique g3 prev-g3) (unique g4 prev-g4)
+
+  ;; For each session, group of X students out of a classroom of N students until the entire classroom is grouped
+  (run 5 [out]
+       (conso students out))
+  
+  (run 20 [out]
+       (fresh [g1 g2 g3 g4]
+       (matche [students]
+               ([[?a ?b ?c ?d ?e . _]] (== g1 ["a " ?a ?b ?c ?d ?e]))
+               ([[_ _ _ _ _ ?a ?b ?c ?d ?e . _]] (== g2 ["b " ?a ?b ?c ?d ?e]))
+               ([[_ _ _ _ _ _ _ _ _ _ ?a ?b ?c ?d ?e . _]] (== g3 ["c " ?a ?b ?c ?d ?e]))
+               ([[_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ?a ?b ?c ?d ?e . _]] (== g4 ["d " ?a ?b ?c ?d ?e])))
+       (== out [g1 g2 g3 g4])))
 
   (run 4 [out]
-       (permuteo out students)
-       (matche [out] ([[_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _]] succeed))
-       )
+       (permo out [:a :b :c :d] ))
+
+  (run 7 (membero :Lark astudents)
+
+  (run 1 [students groups]
+       (fresh
+        [g11 g12 g13 g14 g15
+         g21 g22 g23 g24 g25
+         g31 g32 g33 g34 g35]
+
+        (groupo students g11)
+        (groupo students g12)
+        (groupo students g13)
+        (groupo students g14)
+        (groupo students g15)
+        (groupo students g21)
+        (groupo students g22)
+        (groupo students g23)
+        (groupo students g24)
+        (groupo students g25)
+        (groupo students g31)
+        (groupo students g32)
+        (groupo students g33)
+        (groupo students g34)
+        (groupo students g35)
+        (== [[g11 g12 g13 g14 g15][g21 g22 g23 g24 g25][g31 g32 g33 g34 g35]] groups)
+        ))
+        
+
+  
+  (run 3 [out] (groupo students out))
 
   (let [ct (count students)]
     (run* [q] (even-splito ct 2 q)))
